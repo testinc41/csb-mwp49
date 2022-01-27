68f416a3e5f2186d28e247c4439d906e9219abd0
Code Sandbox <hello@codesandbox.io>
2022-01-27 15:28:40 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c1f06b10f9900cafd79eb28c3b6bbc5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9892d1f66cbc683e5ffb22ed4be8f5",
+    "url": "https://testinc41.github.io/csb-mwp49/index.html"
+  },
+  {
+    "revision": "988c4f417e6766dbd4f5",
+    "url": "https://testinc41.github.io/csb-mwp49/static/css/2.c0e4f2a0.chunk.css"
+  },
+  {
+    "revision": "f144605864256cf1efe2",
+    "url": "https://testinc41.github.io/csb-mwp49/static/css/main.c08d36d4.chunk.css"
+  },
+  {
+    "revision": "988c4f417e6766dbd4f5",
+    "url": "https://testinc41.github.io/csb-mwp49/static/js/2.289ae5b3.chunk.js"
+  },
+  {
+    "revision": "f144605864256cf1efe2",
+    "url": "https://testinc41.github.io/csb-mwp49/static/js/main.6ad662e2.chunk.js"
+  },
+  {
+    "revision": "dce7034b49a6fc59de88",
+    "url": "https://testinc41.github.io/csb-mwp49/static/js/runtime~main.1ee31aa3.js"
+  }
+]);
